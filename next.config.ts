c044f59d338a49b9261f7db8572efe91f6b8b773
alwafint <alwafint@gmail.com>
2026-02-16 01:37:09 +0600

    Fix next.config.ts types

--- next.config.ts
+++ next.config.ts
@@ -1,27 +1,26 @@
 import withPWAInit from "@ducanh2912/next-pwa";
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-    ],
-  },
-};
+import type { NextConfig } from "next"; // এই ইম্পোর্টটি জরুরি
 
 const withPWA = withPWAInit({
   dest: "public",
   cacheOnFrontEndNav: true,
   aggressiveFrontEndNavCaching: true,
   reloadOnOnline: true,
-  // swcMinify: true,  <-- এই লাইনটি মুছে ফেলা হয়েছে কারণ এটি এরর দিচ্ছিল
   disable: process.env.NODE_ENV === "development",
   workboxOptions: {
     disableDevLogs: true,
   },
 });
+
+const nextConfig: NextConfig = { // এখানে টাইপ ডিফাইন করা হয়েছে
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
+};
 
 export default withPWA(nextConfig);
